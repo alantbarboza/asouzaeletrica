c77c912b3cda2fc9a915b0c665ab481ab2b29be8
Alan <67348179+alantbarboza@users.noreply.github.com>
2024-03-12 22:06:40 -0300

    Update feed.js

--- feed.js
+++ feed.js
@@ -1,5 +1,3 @@
-const $ = require('jquery');
-
 $(function(){
     const token = process.env.API_TOKEN
     const url = "https://graph.instagram.com/me/media?access_token=" + token + "&fields=media_url,media_type,caption,permalink";
